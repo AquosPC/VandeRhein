6c32067730c65633d98a306e3327e9c64ae664da
Aquos <48126193+AquosPC@users.noreply.github.com>
2020-11-15 18:24:52 +0100

    change title text

--- Include/PHP/navbar.php
+++ Include/PHP/navbar.php
@@ -1,15 +1,17 @@
-<div class="divtext" >
-  <div><h1>Van de Rhein</h1></div>
-  <div class="pagetext"><h3> <?php
+<ul class="ul-title">
+  <li class="float-left title-text">Van de Rhein</li>
+  <li class="float-right title-text"><?php
     if ($ID == 0) {
       print('Main Page');
     }
     elseif ($ID == 1) {
       print('Database Page');
-    } ?>
-  </h3></div>
-</div>
-<ul>
+    } 
+    elseif ($ID == 2) {
+      print('Login Page');
+    } ?></li>
+</ul>
+<ul class="ul-nav">
   <li><a <?php if ($ID == 0) { print('class="active float-left" '); }  else { print('class="float-left" '); } ?> href="Index">Home</a></li>
   <li><a <?php if ($ID == 1) { print('class="active float-left" ');  } else { print('class="float-left" '); } ?> href="Database">Database</a></li>
   <li><a <?php if ($ID == 2) { print('class="active float-right" ');  } else { print('class="float-right" '); } ?> href="Login">Login</a></li>
