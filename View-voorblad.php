4fbad2d5982aa39afdff36641a2cff296e0e97af
Aquos <48126193+AquosPC@users.noreply.github.com>
2020-12-01 12:54:08 +0100

    added view page for voorblad

--- View-voorblad.php
+++ View-voorblad.php
@@ -23,43 +23,72 @@
         }
     } */
     if (isset($_SESSION['id'])) { ?>
-        <div class="center">
-            <a href="voorbladhijskranentest-aanmaken"><button class="center login-form">Hijskraantest Toevoegen</button></a>
-            <a href="voorbladkabelchecklist-aanmaken"><button class="center login-form">Kabelchecklist Toevoegen</button></a>
-        </div>
         
         </div> <?php
+            $opdrachtnummer = $_GET['opdrachtnummer'];
 
-            $sql = "SELECT * FROM voorbladen;";
+            $sql = "SELECT * FROM voorbladen 
+            INNER JOIN hijstesten ON voorbladen.Opdrachtnummer = hijstesten.Opdrachtnummer";
+
             $result = mysqli_query($conn, $sql);
             $resultcheck = mysqli_num_rows($result); ?>
             <div class="center div-page">
-                <table class="white-text center">
-                    <tbody>
-                        <tr class="table-row">
-                            <td>Opdracht nummer</td>
-                            <td>TCTV nummer</td>
-                            <td>Keuringdatum</td>
-                            <td class="table-divider">Bekijken</td>
-                            <td class="table-divider">Verwijderen</td>
-                        </tr>
                 <?php
-
                 if ($resultcheck > 0) {
                     while ($row = mysqli_fetch_assoc($result)) { ?>
-                    
-                        <tr>
-                            <td><?php echo $row['Opdrachtnummer'] ?></td>
-                            <td><?php echo $row['TCTV_nummer'] ?></td>
-                            <td><?php echo $row['Keuringdatum'] ?></td>
-                            <td><a href="View-voorblad.php?id=<?php echo $row['Opdrachtnummer'] ?>"><button class="button-center">X</button></a></td>
-                            <td><a href="Delete-voorblad.php?id=<?php echo $row['Opdrachtnummer'] ?>"><button class="button-center">X</button></a></td>
-                        </tr>
-                
+                        <hr>
+                        <h1 class="title-text text-left">Voorblad</h1>
+                        <hr>
+                        <p class="voorblad-text">Opdrachtnummer: <?php echo $row['Opdrachtnummer']?></p>
+                        <p class="voorblad-text">TCTV nummer: <?php echo $row['TCTV_nummer']?></p>
+                        <p class="voorblad-text">Keuringdatum: <?php echo $row['Keuringdatum']?></p>
+                        <p class="voorblad-text">Uitvoerder: <?php echo $row['Uitvoerder']?></p>
+                        <p class="voorblad-text">Deskundige: <?php echo $row['Deskundige']?></p>
+                        <p class="voorblad-text">Opstelling kraan: <?php echo $row['Opstelling_Kraan']?></p>
+                        <p class="voorblad-text">Uitvoering Toren Haakhoogte: <?php echo $row['Uitvoering_Toren_Haakhoogte']?></p>
+                        <p class="voorblad-text">Soort Giek: <?php echo $row['Soort_Giek']?></p>
+                        <p class="voorblad-text">Telescoopgiek Delen: <?php echo $row['Telescoopgiek_Delen']?></p>
+                        <p class="voorblad-text">Opbouwgiek Meters: <?php echo $row['Opbouwgiek_Meters']?></p>
+                        <p class="voorblad-text">Hulpgiek Meters: <?php echo $row['Hulpgiek_Meters']?></p>
+                        <p class="voorblad-text">Fly jib Delen: <?php echo $row['Fly_jib_Delen']?></p>
+                        <p class="voorblad-text">Gieklengte: <?php echo $row['Gieklengte']?></p>
+                        <p class="voorblad-text">Topbaar: <?php echo $row['Topbaar']?></p>
+                        <p class="voorblad-text">Loopkat: <?php echo $row['Loopkat']?></p>
+                        <p class="voorblad-text">Verstelbare Giek: <?php echo $row['Verstelbare_giek']?></p>
+                        <p class="voorblad-text">Soort Stempels: <?php echo $row['Soort_Stempels']?></p>
+                        <p class="voorblad-text">Tekortkomingen: <?php echo $row['Tekortkomingen']?></p>
+                        <p class="voorblad-text">Afmelden voor: <?php echo $row['Afmelden_Voor']?></p>
+                        <p class="voorblad-text">Toelichting: <?php echo $row['Toelichting']?></p>
+                        <p class="voorblad-text">Werkinstructie: <?php echo $row['Werkinstructie']?></p>
+                        <p class="voorblad-text">Kabelleverancier: <?php echo $row['Kabelleverancier']?></p>
+                        <p class="voorblad-text">Waarnemingen: <?php echo $row['Waarnemingen']?></p>
+                        <p class="voorblad-text">Handtekening: <?php echo $row['Handtekening']?></p>
+                        <p class="voorblad-text">Aantal Bedrijfsuren: <?php echo $row['Aantal_Bedrijfsuren']?></p>
+                        <p class="voorblad-text">Afleg Redenen: <?php echo $row['Afleg_Redenen']?></p>
+                        <hr>
+                        <h1 class="title-text text-left">Hijstest</h1>
+                        <hr>
+                        <p class="voorblad-text">Volgnummer: <?php echo $row['Volgnummer']?></p>
+                        <p class="voorblad-text">Datum Opgesteld: <?php echo $row['Datum_Opgesteld']?></p>
+                        <p class="voorblad-text">Hoofdgiek Lengte: <?php echo $row['Hoofdgiek_Lengte']?></p>
+                        <p class="voorblad-text">Mech Sectie Gieklengte: <?php echo $row['Mech_Sectie_Gieklengte']?></p>
+                        <p class="voorblad-text">Hulpgiek Lengte: <?php echo $row['Hulpgiek_Lengte']?></p>
+                        <p class="voorblad-text">Hoofdgiek Giekhoek: <?php echo $row['Hoofdgiek_Giekhoek']?></p>
+                        <p class="voorblad-text">Hulpgiek Giekhoek: <?php echo $row['Hulpgiek_Giekhoek']?></p>
+                        <p class="voorblad-text">Hijskabel Aantal Parten: <?php echo $row['Hijskabel_Aantal_Parten']?></p>
+                        <p class="voorblad-text">Zwenkhoek: <?php echo $row['Zwenkhoek']?></p>
+                        <p class="voorblad-text">Eigen Massa Ballast: <?php echo $row['Eigen_Massa_Ballast']?></p>
+                        <p class="voorblad-text">Toelaatbare Bedrijflast: <?php echo $row['Toelaatbare_Bedrijflast']?></p>
+                        <p class="voorblad-text">LMB in Werking: <?php echo $row['LMB_In_Werking']?></p>
+                        <p class="voorblad-text">Proeflast: <?php echo $row['Proeflast']?></p>
+                        <?php if ($row['Akkoord'] = 1) {
+                           echo'<p class="voorblad-text">Akkoord: Ja</p>';
+                        } else {
+                            echo'<p class="voorblad-text">Akkoord: Nee</p>';
+                        } ?>
+                        
                     <?php }
             } ?>
-                </tbody>
-            </table>
         </div>
 
     <?php }
